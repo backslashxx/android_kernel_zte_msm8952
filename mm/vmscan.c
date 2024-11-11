bdec3a0830a4ffeeb32159e778c9da8a2e38b0cb
Alexey Avramov <hakavlad@inbox.lv>
2024-11-12 01:15:10 +0800

    mm/vmscan: add sysctl knobs for protecting clean cache
    The patch provides sysctl knobs for protecting the specified amount of
    clean file pages (CFP) under memory pressure.
    
    The kernel does not have a mechanism for selectively protecting clean file
    pages. A certain amount of the CFP is required by the userspace for normal
    operation. First of all, you need a cache of shared libraries and
    executable files. If the volume of the CFP cache falls below a certain
    level, thrashing and even livelock occurs.
    
    Protection of CFP may be used to prevent thrashing and reducing I/O under
    memory pressure. Hard protection of CFP may be used to avoid high latency
    and prevent livelock in near-OOM conditions. The patch provides sysctl
    knobs for protecting the specified amount of clean file cache under memory
    pressure.
    
    The vm.clean_low_kbytes sysctl knob provides *best-effort* protection of
    CFP. The CFP on the current node won't be reclaimed uder memory pressure
    when their amount is below vm.clean_low_kbytes *unless* we threaten to OOM
    or have no free swap space or vm.swappiness=0. Setting it to a high value
    may result in a early eviction of anonymous pages into the swap space by
    attempting to hold the protected amount of clean file pages in memory. The
    default value is defined by CONFIG_CLEAN_LOW_KBYTES (suggested 0 in
    Kconfig).
    
    The vm.clean_min_kbytes sysctl knob provides *hard* protection of CFP. The
    CFP on the current node won't be reclaimed under memory pressure when their
    amount is below vm.clean_min_kbytes. Setting it to a high value may result
    in a early out-of-memory condition due to the inability to reclaim the
    protected amount of CFP when other types of pages cannot be reclaimed. The
    default value is defined by CONFIG_CLEAN_MIN_KBYTES (suggested 0 in
    Kconfig).
    
    Signed-off-by: Alexey Avramov <hakavlad@inbox.lv>
    
    --
    [backslashxx]
    Adapted from le9db-4.14.patch

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -71,6 +71,19 @@ struct scan_control {
 	/* This context's GFP mask */
 	gfp_t gfp_mask;
 
+	/*
+	 * The clean file pages on the current node won't be reclaimed when
+	 * their amount is below vm.clean_low_kbytes *unless* we threaten
+	 * to OOM or have no free swap space or vm.swappiness=0.
+	 */
+	unsigned int clean_below_low:1;
+
+	/*
+	 * The clean file pages on the current node won't be reclaimed when
+	 * their amount is below vm.clean_min_kbytes.
+	 */
+	unsigned int clean_below_min:1;
+
 	int may_writepage;
 
 	/* Can mapped pages be reclaimed? */
@@ -133,6 +146,17 @@ struct scan_control {
 #else
 #define prefetchw_prev_lru_page(_page, _base, _field) do { } while (0)
 #endif
+
+#if CONFIG_CLEAN_LOW_KBYTES < 0
+#error "CONFIG_CLEAN_LOW_KBYTES must be >= 0"
+#endif
+
+#if CONFIG_CLEAN_MIN_KBYTES < 0
+#error "CONFIG_CLEAN_MIN_KBYTES must be >= 0"
+#endif
+
+unsigned long sysctl_clean_low_kbytes __read_mostly = CONFIG_CLEAN_LOW_KBYTES;
+unsigned long sysctl_clean_min_kbytes __read_mostly = CONFIG_CLEAN_MIN_KBYTES;
 
 /*
  * From 0 .. 100.  Higher means more swappy.
@@ -1967,6 +1991,34 @@ static void get_scan_count(struct lruvec *lruvec, struct scan_control *sc,
 	if (!global_reclaim(sc))
 		force_scan = true;
 
+	/*
+	 * Check the number of clean file pages to protect them from
+	 * reclaiming if their amount is below the specified.
+	 */
+	if (sysctl_clean_low_kbytes || sysctl_clean_min_kbytes) {
+		unsigned long reclaimable_file, dirty, clean;
+
+		reclaimable_file =
+			node_page_state(pgdat, NR_ACTIVE_FILE) +
+			node_page_state(pgdat, NR_INACTIVE_FILE) +
+			node_page_state(pgdat, NR_ISOLATED_FILE);
+		dirty = node_page_state(pgdat, NR_FILE_DIRTY);
+		/*
+		 * node_page_state() sum can go out of sync since
+		 * all the values are not read at once.
+		 */
+		if (likely(reclaimable_file > dirty))
+			clean = (reclaimable_file - dirty) << (PAGE_SHIFT - 10);
+		else
+			clean = 0;
+
+		sc->clean_below_low = clean < sysctl_clean_low_kbytes;
+		sc->clean_below_min = clean < sysctl_clean_min_kbytes;
+	} else {
+		sc->clean_below_low = false;
+		sc->clean_below_min = false;
+	}
+
 	/* If we have no swap space, do not bother scanning anon pages. */
 	if (!sc->may_swap || (get_nr_swap_pages() <= 0)) {
 		scan_balance = SCAN_FILE;
@@ -2012,6 +2064,15 @@ static void get_scan_count(struct lruvec *lruvec, struct scan_control *sc,
 			scan_balance = SCAN_ANON;
 			goto out;
 		}
+	}
+ 	/*
+	 * Force-scan anon if clean file pages is under vm.clean_min_kbytes
+	 * or vm.clean_low_kbytes (unless the swappiness setting
+	 * disagrees with swapping).
+	 */
+	if ((sc->clean_below_low || sc->clean_below_min) && vm_swappiness) {
+		scan_balance = SCAN_ANON;
+		goto out;
 	}
 
 	/*
@@ -2103,6 +2164,13 @@ out:
 			/* Look ma, no brain */
 			BUG();
 		}
+		/*
+		 * Don't reclaim clean file pages when their amount is below
+		 * vm.clean_min_kbytes.
+		 */
+		if (file && sc->clean_below_min)
+			scan = 0;
+
 		nr[lru] = scan;
 	}
 }
